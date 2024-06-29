34030305e040ecfc3ef55068537b03c27f063068
sunnovation <poojapawandev96@gmail.com>
2024-06-29 12:14:04 +0530

    Modify Controller

--- src/main/java/com/test/controller/TestAPIController.java
+++ src/main/java/com/test/controller/TestAPIController.java
@@ -1,10 +1,14 @@
 package com.test.controller;
 
 import com.test.entity.Employee;
+import com.test.request.FetchDataRequest;
 import com.test.service.TestAPIService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -20,5 +24,10 @@ public class TestAPIController {
     @GetMapping("/fetch")
     public List<Employee> fetchAllEmployee(){
         return testAPIService.fetchAllEmployees();
+    }
+
+    @GetMapping("/fetchDetail")
+    public ResponseEntity<HashMap<String,Object>> fetchAllEmployee(@RequestBody FetchDataRequest request){
+        return new ResponseEntity<>(testAPIService.fetchEmployeeDetails(request), HttpStatus.OK);
     }
 }
